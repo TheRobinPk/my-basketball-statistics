642a247bfdb0b2fa25dd3fb80aa97e060d4da497
Zoltan Csenyi <cszoltan422@gmail.com>
2022-07-13 09:25:36 +0200

    BSA-3 - add ShootAroundEntity with shoot-around-create-table.ts

---
+++ src/domain/migrations/shoot-around-create-table.ts
@@ -0,0 +1,44 @@
+import {MigrationInterface, QueryRunner, Table} from 'typeorm';
+
+export class ShootAroundCreateTable1608217149351 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'shoot_around',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'integer',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'timestamp',
+                        type: 'int',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'total_attempts',
+                        type: 'int',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'made_attempts',
+                        type: 'int',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'spot',
+                        type: 'string',
+                        isNullable: false,
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('shoot_around');
+    }
+}
--- src/domain/shoot-around.ts
+++ src/domain/shoot-around.ts
@@ -1,22 +1,43 @@
 import {Moment} from 'moment';
+import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
 
-export const PAINT = 'PAINT';
-export const FREE_THROW = 'FREE_THROW';
-export const MID_RANGE_LEFT_CORNER = 'MID_RANGE_LEFT_CORNER';
-export const MID_RANGE_RIGHT_CORNER = 'MID_RANGE_RIGHT_CORNER';
-export const MID_RANGE_LEFT_WING = 'MID_RANGE_LEFT_WING';
-export const MID_RANGE_RIGHT_WING = 'MID_RANGE_RIGHT_WING';
-export const MID_RANGE_HIGH_POST = 'MID_RANGE_HIGH_POST';
-export const THREE_POINT_LEFT_CORNER = 'THREE_POINT_LEFT_CORNER';
-export const THREE_POINT_RIGHT_CORNER = 'THREE_POINT_RIGHT_CORNER';
-export const THREE_POINT_LEFT_WING = 'THREE_POINT_LEFT_WING';
-export const THREE_POINT_RIGHT_WING = 'THREE_POINT_RIGHT_WING';
-export const THREE_POINT_TOP_OF_THE_KEY = 'THREE_POINT_TOP_OF_THE_KEY';
+export enum ShootAroundSpot {
+    PAINT = 'PAINT',
+    FREE_THROW = 'FREE_THROW',
+    MID_RANGE_LEFT_CORNER = 'MID_RANGE_LEFT_CORNER',
+    MID_RANGE_RIGHT_CORNER = 'MID_RANGE_RIGHT_CORNER',
+    MID_RANGE_LEFT_WING = 'MID_RANGE_LEFT_WING',
+    MID_RANGE_RIGHT_WING = 'MID_RANGE_RIGHT_WING',
+    MID_RANGE_HIGH_POST = 'MID_RANGE_HIGH_POST',
+    THREE_POINT_LEFT_CORNER = 'THREE_POINT_LEFT_CORNER',
+    THREE_POINT_RIGHT_CORNER = 'THREE_POINT_RIGHT_CORNER',
+    THREE_POINT_LEFT_WING = 'THREE_POINT_LEFT_WING',
+    THREE_POINT_RIGHT_WING = 'THREE_POINT_RIGHT_WING',
+    THREE_POINT_TOP_OF_THE_KEY = 'THREE_POINT_TOP_OF_THE_KEY'
+}
+
+@Entity('shoot_around')
+export class ShootAroundEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ nullable: false, name: 'timestamp' })
+    timestamp!: number;
+
+    @Column({ nullable: false, name: 'total_attempts' })
+    totalAttempts!: number;
+
+    @Column({ nullable: false, name: 'made_attempts' })
+    madeAttempts!: number;
+
+    @Column({ nullable: false, name: 'spot' })
+    spot!: string;
+}
 
 export interface ShootAround {
     id?: number;
     dateTime: Moment;
     totalAttempts: number;
     madeAttempts: number;
-    spot: string;
+    spot: ShootAroundSpot;
 }
