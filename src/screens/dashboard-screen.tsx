9732eeb65cdaab3be9a85443675288afd2a18d64
Zoltan Csenyi <cszoltan422@gmail.com>
2022-07-14 15:07:28 +0200

    BSA-5 - navigate to add-shootaround-screen from dashboard-screen.tsx

--- src/screens/dashboard-screen.tsx
+++ src/screens/dashboard-screen.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import FloatingActionButton from '../components/common/floating-action-button/floating-action-button';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../navigation/application-navigator';
 import colors from '../colors';
 
-const DashboardScreen = () => {
+type IProps = NativeStackScreenProps<RootStackParamList, 'DashboardScreen'>;
+
+const DashboardScreen = (props: IProps) => {
     return (
         <View style={styles.container}>
             <Text>
                 DashboardScreen
             </Text>
+            <FloatingActionButton
+                color={colors.primaryColor}
+                icon={{ name: 'add', color: colors.white }}
+                openIcon={{ name: 'close', color: colors.white }}
+                actions={[
+                    {
+                        title: 'Add Shootaround',
+                        icon: { name: 'add', color: colors.white },
+                        color: colors.primaryColor,
+                        pressHandler: () => props.navigation.navigate('AddShootAroundScreen')
+                    }
+                ]} />
         </View>
     );
 };
