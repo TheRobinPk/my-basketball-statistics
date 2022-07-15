f5c468b6ea07ea08c94cb4a445c9ab12fcb8fd28
Zoltan Csenyi <cszoltan422@gmail.com>
2022-07-15 12:10:45 +0200

    BSA-5 - add App.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,20 @@
 import 'reflect-metadata';
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import ApplicationRoot from './src/application-root';
 import Toast from 'react-native-toast-message';
 import store from './src/redux/store/store';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
         <NavigationContainer>
             <Provider store={store}>
                 <ApplicationRoot />
             </Provider>
-            <Toast ref={(ref) => Toast.setRef(ref)} />
+            <Toast />
         </NavigationContainer>
     );
-  }
-}
+};
 
 export default App;
