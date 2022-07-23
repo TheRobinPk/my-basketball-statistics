dbfd6c253713466410be10a9ccf38080bbc25536
Zoltan Csenyi <cszoltan422@gmail.com>
2022-07-23 11:57:04 +0200

    BSA-7 - finalize add-shoot-around-screen.tsx

--- src/screens/add-shoot-around-screen.tsx
+++ src/screens/add-shoot-around-screen.tsx
@@ -3,6 +3,7 @@ import {StyleSheet, Text, TextInput, View} from 'react-native';
 import BasketballCourt from '../components/basketball-court/basketball-court';
 import Button from '../components/common/button/button';
 import Snackbar from '../components/common/snackbar/snackbar';
+import { MaterialIcons } from '@expo/vector-icons';
 import {useAppDispatch, useAppSelector} from '../redux/store/store';
 import {ShootAroundSpot} from '../domain/shoot-around';
 import {
@@ -21,7 +22,6 @@ const AddShootAroundScreen = () => {
     const isLoading = useAppSelector(state => state.addShootAround.isLoading);
     const submitDisabled = useAppSelector(state => state.addShootAround.submitDisabled);
     const submitSuccess = useAppSelector(state => state.addShootAround.submitSuccess);
-    const error = useAppSelector(state => state.addShootAround.error);
 
     const dispatch = useAppDispatch();
 
@@ -30,76 +30,85 @@ const AddShootAroundScreen = () => {
     });
 
     return (
-        <View testID='add-shoot-around-container' style={styles.container}>
-            <BasketballCourt
-                selectedSpot={shootAroundSpot}
-                onSpotSelected={(selectedSpot: ShootAroundSpot) => {
-                    dispatch(setShootAroundFormValues({
-                        totalAttempts: totalAttempts,
-                        madeAttempts: madeAttempts,
-                        shootAroundSpot: selectedSpot
-                    }));
-                }} />
-            <View style={styles.rowContainer}>
-                <View style={styles.columnContainer}>
-                    <Text
-                        testID='total-attempts-text-input-title'
-                        style={styles.labelTitle}>
-                        Total Attempts
-                    </Text>
-                    <TextInput
-                        testID='total-attempts-text-input'
-                        style={styles.textInputStyle}
-                        editable
-                        placeholder='Total Attempts...'
-                        keyboardType='numeric'
-                        autoCapitalize='none'
-                        contextMenuHidden
-                        value={totalAttempts?.toString()}
-                        onChangeText={(value) => {
-                            dispatch(setShootAroundFormValues({
-                                totalAttempts: value,
-                                madeAttempts: madeAttempts,
-                                shootAroundSpot: shootAroundSpot
-                            }));
-                        }} />
-                </View>
-                <View style={styles.columnContainer}>
-                    <Text
-                        testID='made-attempts-text-input-title'
-                        style={styles.labelTitle}>
-                        Made Attempts
-                    </Text>
-                    <TextInput
-                        testID='made-attempts-text-input'
-                        style={styles.textInputStyle}
-                        editable
-                        placeholder='Made Attempts...'
-                        keyboardType='numeric'
-                        autoCapitalize='none'
-                        contextMenuHidden
-                        value={madeAttempts?.toString()}
-                        onChangeText={(value) => {
+        <>
+            <View testID='add-shoot-around-container' style={styles.container}>
+                <View>
+                    <BasketballCourt
+                        selectedSpot={shootAroundSpot}
+                        onSpotSelected={(selectedSpot: ShootAroundSpot) => {
                             dispatch(setShootAroundFormValues({
                                 totalAttempts: totalAttempts,
-                                madeAttempts: value,
-                                shootAroundSpot: shootAroundSpot
+                                madeAttempts: madeAttempts,
+                                shootAroundSpot: selectedSpot
                             }));
                         }} />
+                    {(shootAroundSpot === undefined) && (
+                        <View style={styles.basketballCourtInfoStyle}>
+                            <MaterialIcons name='info' size={18} color={colors.primaryColor} />
+                            <Text testID='basketball-court-info-text' style={styles.basketballCourtInfoTextStyle}>
+                                You can select a shooting spot by tapping on the spot
+                            </Text>
+                        </View>
+                    )}
+                    <View style={styles.rowContainer}>
+                        <View style={styles.columnContainer}>
+                            <Text
+                                testID='total-attempts-text-input-title'
+                                style={styles.labelTitle}>
+                                Total Attempts
+                            </Text>
+                            <TextInput
+                                testID='total-attempts-text-input'
+                                style={styles.textInputStyle}
+                                editable
+                                placeholder='Total Attempts...'
+                                keyboardType='numeric'
+                                autoCapitalize='none'
+                                contextMenuHidden
+                                value={totalAttempts?.toString()}
+                                onChangeText={(value) => {
+                                    dispatch(setShootAroundFormValues({
+                                        totalAttempts: value,
+                                        madeAttempts: madeAttempts,
+                                        shootAroundSpot: shootAroundSpot
+                                    }));
+                                }} />
+                        </View>
+                        <View style={styles.columnContainer}>
+                            <Text
+                                testID='made-attempts-text-input-title'
+                                style={styles.labelTitle}>
+                                Made Attempts
+                            </Text>
+                            <TextInput
+                                testID='made-attempts-text-input'
+                                style={styles.textInputStyle}
+                                editable
+                                placeholder='Made Attempts...'
+                                keyboardType='numeric'
+                                autoCapitalize='none'
+                                contextMenuHidden
+                                value={madeAttempts?.toString()}
+                                onChangeText={(value) => {
+                                    dispatch(setShootAroundFormValues({
+                                        totalAttempts: totalAttempts,
+                                        madeAttempts: value,
+                                        shootAroundSpot: shootAroundSpot
+                                    }));
+                                }} />
+                        </View>
+                    </View>
+                </View>
+                <View style={styles.submitButtonContainerStyle}>
+                    <Button
+                        label='Submit'
+                        testID='add-shoot-around-submit-button'
+                        labelTestID='add-shoot-around-submit-button-label'
+                        disabled={submitDisabled}
+                        loading={isLoading}
+                        onPress={() => dispatch(submitShootAround())} />
                 </View>
             </View>
-            {error.isPresent && (
-                <View>
-                    <Text testID='add-shoot-around-error-text'>{error.text}</Text>
-                </View>
-            )}
-            <Button
-                label='Submit'
-                testID='add-shoot-around-submit-button'
-                labelTestID='add-shoot-around-submit-button-label'
-                disabled={submitDisabled}
-                loading={isLoading}
-                onPress={() => dispatch(submitShootAround())} />
             <Snackbar
                 label='Saved successfully'
                 testID='add-shoot-success-snackbar'
@@ -107,14 +116,14 @@ const AddShootAroundScreen = () => {
                 visible={submitSuccess}
                 duration={2000}
                 onDismiss={() => dispatch(setAddShootAroundSubmitSuccess(false))} />
-        </View>
+        </>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         height: '100%',
-        padding: 8
+        justifyContent: 'space-between'
     },
     labelTitle: {
         fontWeight: 'bold',
@@ -122,10 +131,8 @@ const styles = StyleSheet.create({
         fontSize: 16
     },
     textInputStyle: {
-        color: colors.grey,
         fontSize: 14,
         borderBottomWidth: 1,
-        borderColor: colors.grey,
         marginEnd: 16,
         height: 32
     },
@@ -134,11 +141,31 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap',
         justifyContent: 'space-between',
         alignContent: 'flex-start',
-        marginBottom: 8
+        paddingLeft: 8,
+        paddingRight: 8,
+        marginTop: 8
     },
     columnContainer: {
         width: '50%'
     },
+    basketballCourtInfoStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: 8,
+        paddingRight: 8,
+        marginTop: 4
+    },
+    basketballCourtInfoTextStyle: {
+        color: colors.primaryColor,
+        marginLeft: 4,
+        fontSize: 12,
+        fontStyle: 'italic'
+    },
+    submitButtonContainerStyle: {
+        width: 160,
+        alignSelf: 'center',
+        marginBottom: 16
+    }
 });
 
 export default AddShootAroundScreen;
