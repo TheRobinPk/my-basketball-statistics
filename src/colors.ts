d2cf94a6d5c3a4a7eae11f68920b9e61fb365a5d
Zoltan Csenyi <cszoltan422@gmail.com>
2022-07-23 09:21:51 +0200

    BSA-16 - update colors.ts

--- src/colors.ts
+++ src/colors.ts
@@ -1,10 +1,8 @@
 export default {
     primaryColor: '#81aba8',
     accentColor: '#c48b9f',
-    placeholderGrey: '#cfd8dc',
     white: '#ffffff',
     grey: '#888',
-    lightGrey: '#eceff1',
+    disabledGrey: '#bdbdbd',
     black: '#000000',
-    errorRed: '#ff7b7b'
 };
