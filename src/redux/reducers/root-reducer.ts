6e5e9ae55f7adc77d3f7dbca5963c2e9221ef97d
Zoltan Csenyi <cszoltan422@gmail.com>
2022-08-07 18:53:05 +0200

    BSA-9 - update redux setup to include newly added components

--- src/redux/reducers/root-reducer.ts
+++ src/redux/reducers/root-reducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import applicationReducer from './application/application-reducer';
 import addShootAroundReducer from './add-shootaround/add-shootaround-reducer';
 import dashboardReducer from './dashboard/dashboard-reducer';
+import shootAroundListReducer from './shootaround-list/shootaround-list-reducer';
 
 const rootReducer = combineReducers({
     application: applicationReducer,
     addShootAround: addShootAroundReducer,
-    dashboard: dashboardReducer
+    dashboard: dashboardReducer,
+    shootAroundList: shootAroundListReducer
 });
 
 export default rootReducer;
--- src/redux/sagas/root-saga.ts
+++ src/redux/sagas/root-saga.ts
@@ -2,11 +2,13 @@ import { all } from 'redux-saga/effects';
 import {applicationMountedWatcherSaga} from './application/application-saga';
 import {submitShootAroundWatcherSaga} from './add-shootaround/add-shootaround-saga';
 import {dashboardDateRangeChangedWatcherSaga} from './dashboard/dashboard-saga';
+import {getShootAroundListDataWatcherSaga} from './shootaround-list/shootaround-list-saga';
 
 export default function* rootSaga() {
     yield all([
         applicationMountedWatcherSaga(),
         submitShootAroundWatcherSaga(),
-        dashboardDateRangeChangedWatcherSaga()
+        dashboardDateRangeChangedWatcherSaga(),
+        getShootAroundListDataWatcherSaga()
     ]);
 }
