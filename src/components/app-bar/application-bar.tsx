af5ad5662e7c463d3e6c872571621cdbaf454aa5
Zoltan Csenyi <cszoltan422@gmail.com>
2022-08-07 22:45:24 +0200

    BSA-30 - app application-bar.tsx

---
+++ src/components/app-bar/application-bar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import colors from '../../colors';
+import {Appbar} from 'react-native-paper';
+import {DrawerActions, useNavigation} from '@react-navigation/native';
+import {StyleSheet} from 'react-native';
+
+interface IProps {
+    title: string;
+    showDrawerToggle?: boolean;
+    showGoBack?: boolean;
+}
+
+const ApplicationBar = (props: IProps) => {
+    const navigation = useNavigation();
+
+    return (
+        <Appbar style={styles.appBarStyle} mode='small'>
+            {props.showGoBack && (
+                <Appbar.Action
+                    icon='arrow-left'
+                    iconColor={colors.white}
+                    onPress={() => navigation.goBack()} />
+            )}
+            {props.showDrawerToggle && (
+                <Appbar.Action
+                    icon='menu'
+                    iconColor={colors.white}
+                    onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)} />
+            )}
+            <Appbar.Content title={props.title} titleStyle={{ color: colors.white }} />
+        </Appbar>
+    );
+};
+
+const styles = StyleSheet.create({
+    appBarStyle: {
+        backgroundColor: colors.primaryColor
+    }
+});
+
+export default ApplicationBar;
