6fe82f553e8b7dbc605428242cb2f7f734db33da
Zoltan Csenyi <cszoltan422@gmail.com>
2022-08-07 22:45:41 +0200

    BSA-30 - update shoot-around-stack-navigator.tsx

--- src/navigation/shoot-around-stack-navigator.tsx
+++ src/navigation/shoot-around-stack-navigator.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import colors from '../colors';
 import DashboardScreen from '../screens/shoot-around/dashboard-screen';
 import AddShootAroundScreen from '../screens/shoot-around/add-shoot-around-screen';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import DrawerToggle from './drawer-toggle';
 
 export type ShootAroundStackNavigatorParamList = {
     DashboardScreen: undefined;
@@ -16,25 +14,14 @@ const ShootAroundDashboardStackNavigator = () => {
     return (
         <Stack.Navigator
             screenOptions={{
-                headerStyle: {
-                    backgroundColor: colors.primaryColor
-                },
-                headerTintColor: colors.white
+                headerShown: false
         }}>
             <Stack.Screen
                 name='DashboardScreen'
-                component={DashboardScreen}
-                options={{
-                    title: 'Dashboardv2',
-                    // eslint-disable-next-line react/no-multi-comp
-                    headerLeft: () => <DrawerToggle />
-                }} />
+                component={DashboardScreen} />
             <Stack.Screen
                 name='AddShootAroundScreen'
-                component={AddShootAroundScreen}
-                options={{
-                    title: 'Add Shoot Around'
-                }} />
+                component={AddShootAroundScreen} />
         </Stack.Navigator>
     );
 
