5f5db8dd69a32a77e6b0705fc0e693a91beaa43e
Zoltan Csenyi <cszoltan422@gmail.com>
2022-08-20 15:01:41 +0200

    BSA-41 - finish up shoot-around-regression.feature

--- e2e/step-definitions/step-definitions.ts
+++ e2e/step-definitions/step-definitions.ts
@@ -1,4 +1,4 @@
-import {BeforeAll, Given, Then, setDefaultTimeout} from '@cucumber/cucumber';
+import {BeforeAll, Given, When, Then, setDefaultTimeout} from '@cucumber/cucumber';
 import {remote} from 'webdriverio';
 import {getElementByClassName, getElementByResourceId, getElementByText} from '../utils/get-element';
 import {androidConfig} from '../config/e2e-config';
@@ -17,22 +17,17 @@ Given(/^I wait for (\d+) milliseconds$/, async (millis: number) => {
     await driver.pause(millis);
 });
 
-Given(/^I tap on "(.*)"$/, async (id: string) => {
+When(/^I tap on "(.*)"$/, async (id: string) => {
     const element = await getElementByResourceId(id, driver);
     await element.click();
 });
 
-Given(/^I tap on "(.*)" text$/, async (text: string) => {
+When(/^I tap on "(.*)" text$/, async (text: string) => {
     const element = await getElementByText(text, driver);
     await element.click();
 });
 
-Then(/^I except "(.*)" view type exists$/, async (className: string) => {
-    const element = await getElementByClassName(className, driver);
-    await element.click(); // click should fail if not exists
-});
-
-Given('I tap on the basketball court', async () => {
+When('I tap on the basketball court', async () => {
     const { width, height } = await driver.getWindowSize();
     await driver.touchPerform([
         {
@@ -45,12 +40,17 @@ Given('I tap on the basketball court', async () => {
     ]);
 });
 
-Given(/^I set the "(.*)" text input with value (\d+)$/, async (id: string, value: number) => {
+When(/^I set the "(.*)" text input with value (\d+)$/, async (id: string, value: number) => {
     const totalAttemptsInput = await getElementByResourceId(id, driver);
     await totalAttemptsInput.setValue(value);
 });
 
-Then(/^I expect "(.*)" text is visible on the screen$/, async (text: string) => {
+Then(/^I except "(.*)" view type exists$/, async (className: string) => {
+    const element = await getElementByClassName(className, driver);
+    await element.click(); // click should fail if not exists
+});
+
+Then(/^I expect "(.*)" text is visible$/, async (text: string) => {
     const elementWithText = await getElementByText(text, driver);
     await expectHasText(elementWithText, text);
 });
